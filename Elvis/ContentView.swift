2ba7ff3d388cd3c32a7c6ce298c449c9b82e1799
Derek Meyers <meyers.derek@icloud.com>
2026-03-13 17:30:12 -0400

    First commit.

--- Elvis/ContentView.swift
+++ Elvis/ContentView.swift
@@ -8,15 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message = ""
+    @State private var imageName = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("What's So Funny 'Bout?")
+                .foregroundStyle(.purple)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .multilineTextAlignment(.center)
+            
+                Spacer()
+            
+            Image(systemName: imageName)
+                .resizable()
+                .frame(width: 300, height: 300)
+                .foregroundStyle(.black)
+            Text(message)
+                .foregroundStyle(.purple)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace") {
+                    imageName = "peacesign"
+                    message = "PEACE"
+                }
+                Button("Love") {
+                    imageName = "heart.circle"
+                    message = "LOVE"
+                }
+                Button("Understanding") {
+                    imageName = "lightbulb.circle"
+                    message = "UNDERSTANDING"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.purple)
+            
         }
         .padding()
     }
+    
 }
 
 #Preview {
