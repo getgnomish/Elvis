e6fdb374bda325dbc5697efc7fbf8433e0c3a1c4
Derek Meyers <meyers.derek@icloud.com>
2026-03-13 16:55:21 -0400

    Initial Commit

---
+++ Elvis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Elvis
+//
+//  Created by Derek Meyers on 3/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Derek Meyers on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
